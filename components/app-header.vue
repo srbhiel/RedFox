5ed190840f8e58a218ba3743a3985a0939ed01b0
srbhiel <srbhiel.yt@gmail.com>
2026-08-26 00:49:54 -0300

    update components/app-header.vue

---
+++ components/app-header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <NuxtLink to="/">
+        <img src="/logo.png" alt="11b" />
+      </NuxtLink>
+    </div>
+
+    <div class="header__actions">
+      <button type="button" class="btn btn--register" @click="$emit('register')">
+        Registrar
+      </button>
+      <button type="button" class="btn btn--login" @click="$emit('login')"
+        Entrar
+      </button>
+    </div>
+  </header>
+</template>
+
+<script setup>
+defineEmits(['register', 'login'])
+</script>
